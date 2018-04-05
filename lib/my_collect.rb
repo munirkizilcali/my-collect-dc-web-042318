a54f25bb98d62d289bd3f2f14db0a3c05aad1ad1
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-05 12:58:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+    index = 0
+    new_array = []
+    while index < array.length do
+        new_array.push(yield array[index])
+        index += 1
+    end
+    new_array
+end
 
 
